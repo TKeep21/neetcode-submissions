bbb55348830d201b30711644183407f83ee03edd
Магомедов Махач <87029873+TKeep21@users.noreply.github.com>
2026-08-24 15:32:55 +0300

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int needed = target - nums[i];
+
+            if (seen.containsKey(needed)) {
+                return new int[] {seen.get(needed), i};
+            }
+
+            seen.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
